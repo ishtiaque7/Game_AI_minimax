07620e8e8f6b6413dbc1d1c44619fee2b482cd1d
Ishtiaque Ahmed <93021890+ishtiaque7@users.noreply.github.com>
2023-11-02 18:42:03 +0600

    Add files via upload

---
+++ AI for tik tak toe/const.py
@@ -0,0 +1,21 @@
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+
+s_size=WIDTH//COLS
+line_width= 10
+circle_width=15
+RADIUS = s_size//4
+cross_width = 15
+
+OFFSET = 50
+#colour 
+
+bg_color = (28,170,156)
+line_color = (23,145,135)
+cross_color = (66,66,66)
+circle_color=(239,231,200)
+wins_color = (239, 231, 200)
+cros_color = (66, 66, 66)
